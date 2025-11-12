109166685753a34d442698b726622b858ab82f2a
chuhannn26 <chuhannn26@gmail.com>
2025-11-12 21:01:23 +0800

    1112workshop

---
+++ script.js
@@ -0,0 +1,66 @@
+const projectItems = document.querySelectorAll(".project-item");
+
+projectItems.forEach(item => {
+    item.addEventListener("click", function() {
+        const url = this.getAttribute("data-url");
+        if (url) {
+            window.open(url, "_blank");
+        }
+    });
+});
+
+// Typewriter effect
+const typewriterElement = document.querySelector(".typewriter");
+const texts = ["NYCU Software Development Club.", "Core System.", "Clustron.", "HPC.", "Commonground."];
+let textIndex = 0;
+let isDeleting = false;
+
+function typeWriter() {
+    const currentText = texts[textIndex];
+    const typingSpeed = 100; 
+    const deletingSpeed = 50; 
+    const pauseAfterTyping = 2000;
+    const pauseAfterDeleting = 500;
+    
+    if (!isDeleting) {
+    // typing phase
+    let charIndex = 0;
+    typewriterElement.textContent = '';
+    
+    const typingInterval = setInterval(() => {
+        typewriterElement.textContent += currentText[charIndex];
+        charIndex++;
+        
+        if (charIndex === currentText.length) { // finished typing
+        clearInterval(typingInterval);
+        // pause after typing, then start deleting
+        setTimeout(() => {
+            isDeleting = true;
+            typeWriter();
+        }, pauseAfterTyping);
+        }
+    }, typingSpeed);
+    
+    } else {
+    // deleting phase
+    let charIndex = currentText.length;
+    
+    const deletingInterval = setInterval(() => {
+        typewriterElement.textContent = currentText.substring(0, charIndex - 1);
+        charIndex--;
+        
+        if (charIndex === 0) {
+        clearInterval(deletingInterval);
+        isDeleting = false;
+        textIndex = (textIndex + 1) % texts.length;
+        
+        setTimeout(() => {
+            typeWriter();
+        }, pauseAfterDeleting);
+        }
+    }, deletingSpeed);
+    }
+}
+
+// Start the typewriter effect
+typeWriter();
